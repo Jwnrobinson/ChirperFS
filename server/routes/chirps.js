9eb1e294daf2bca11bf3eda439c109e5b346f313
Juwan Robinson <juwanrobinson@Sharis-Air.attlocal.net>
2021-11-04 09:13:55 -0500

    Last Commit

---
+++ server/routes/chirps.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const chirpsStore = require('../filestore');
+
+let router = express.Router();
+
+router.get('/id?', (req, res) => {
+    let id = req.params.id
+    if(id){
+        res.json(chirpsStore.GetChirps(req));
+    } else {
+        res.send(chirpsStore.GetChirps());
+    }
+
+
+
+});
+
+router.post('/', (req, res) => {
+    chirpsStore.CreateChirp(req.body);
+    res.sendStatus(200);
+});
+
+module.exports = router;
